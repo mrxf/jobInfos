968d516ccd4d69a562a84792262d0bf69173dad6
mrxf <zhshy123@foxmail.com>
2016-07-08 00:30:08 +0800

    提交测试的文件

--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/info', function(req, res, next) {
   var info = {
     username : 'trinity',
     password : 'girlisyou'
   }
   res.json(info);
 });
+router.get('/',function(req,res,next) {
+  res.render('user',{title:'用户信息'})
+})
 
 module.exports = router;
